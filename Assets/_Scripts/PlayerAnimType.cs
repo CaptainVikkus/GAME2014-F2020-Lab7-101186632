17ae4c1e0d5f9a2ec4105ea57ccf349bdc58e6c6
CaptainVikkus <jaketreleaven@gmail.com>
2020-11-13 21:25:22 -0500

    Crouching Player and Enemy Character

---
+++ Assets/_Scripts/PlayerAnimType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PlayerAnimType
+{
+    IDLE,
+    RUN,
+    JUMP,
+    CROUCH
+}
